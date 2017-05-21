0336b0d72b5e5e7813a599232d2c7969429b9309
Inceptor <inceptor@github.com>
2017-05-22 00:52:06 +0200

    Adding aws-getTokenMFA in cli (no arg) and fix bug if the MFA token is wrong

--- aws-getTokenMFA.sh
+++ aws-getTokenMFA.sh
@@ -16,64 +16,91 @@ if ! hash pcregrep 2>/dev/null; then
   exit 1
 fi
 
+#Check if the script is use with arg
+if [ $# -gt 3 ]; then
+  echo "Usage :"
+  echo "$0 MFAtoken [profileName] [profileToFetchCredentials]"
+  echo "TIPS: You can launch the script with interactive mode (no arg)."
+  exit 1
+elif [ $# -gt 0 ]; then
+  tokenMFA="$1"
+
+  if ! [ -z $2 ]; then
+    pname="$2"
+  fi
+
+  if ! [ -z $3 ]; then
+    profile="$3"
+  fi
+  interactiveMode=false
+else
+  interactiveMode=true
+fi
+
 #Retrive some file location
 credentialFileLocation=${AWS_SHARED_CREDENTIALS_FILE};
 if [ -z "$credentialFileLocation" ]; then
     credentialFileLocation=~/.aws/credentials
 fi
-
+  
 configFileLocation=${AWS_CONFIG_FILE};
 if [ -z "$configFileLocation" ]; then
     configFileLocation=~/.aws/config
 fi
 
-#Cleanning conf file
-read -p "Do you want to list MFA profile expired (Y/N)?" clean
-
-#Find expired session using token only
-if [ "$clean" == "Y" ] || [ "$clean" == "y" ]; then
-  matching=$(pcregrep -M "\[.*\]\n((aws_access_key_id|aws_secret_access_key).*\n)*aws_session_token.*" "$credentialFileLocation" | grep "\[.*\]" | sed "s/[]]//g" | sed "s/[[]//g")
-  if ! [ -z "$matching" ]; then
-    echo "Searching expired mfa profile :"
-    matchingExpired=()
-    while read line; do
-        if ! aws sts get-caller-identity --profile $line 2>/dev/null 1>/dev/null; then
-          echo "EXPIRATED : $line";
-          matchingExpired+=("$line")
+if [ $interactiveMode == "true" ]; then  
+  #Cleanning conf file
+  read -p "Do you want to list MFA profile expired (Y/N)?" clean
+  
+  #Find expired session using token only
+  if [ "$clean" == "Y" ] || [ "$clean" == "y" ]; then
+    matching=$(pcregrep -M "\[.*\]\n((aws_access_key_id|aws_secret_access_key).*\n)*aws_session_token.*" "$credentialFileLocation" | grep "\[.*\]" | sed "s/[]]//g" | sed "s/[[]//g")
+    if ! [ -z "$matching" ]; then
+      echo "Searching expired mfa profile :"
+      matchingExpired=()
+      while read line; do
+          if ! aws sts get-caller-identity --profile $line 2>/dev/null 1>/dev/null; then
+            echo "EXPIRATED : $line";
+            matchingExpired+=("$line")
+          fi
+      done <<< "$matching";
+  
+      if [ ${#matchingExpired[@]} -ne 0 ]; then
+        #delete expired session
+        read -p "Do you want to delete ALL the above profile ?! (Yes)?" clean
+        if [ "$clean" == "Yes" ]; then
+          for (( i=0; i < ${#matchingExpired[@]}; i++ )); do
+            echo "$(pcregrep -Mv "\[profile ${matchingExpired[i]}\]\n(region|output).*(\n)?" "$configFileLocation")" > "$configFileLocation"
+            echo "$(pcregrep -Mv "\[${matchingExpired[i]}\]\n((aws_access_key_id|aws_secret_access_key).*\n)*aws_session_token.*" "$credentialFileLocation")" > "$credentialFileLocation"
+          done
+          echo "ALL profiles are deleted."
+        else
+          echo "No profile deleted."
         fi
-    done <<< "$matching";
-
-    if [ ${#matchingExpired[@]} -ne 0 ]; then
-      #delete expired session
-      read -p "Do you want to delete ALL the above profile ?! (Yes)?" clean
-      if [ "$clean" == "Yes" ]; then
-        for (( i=0; i < ${#matchingExpired[@]}; i++ )); do
-          echo "$(pcregrep -Mv "\[profile ${matchingExpired[i]}\]\n(region|output).*(\n)?" "$configFileLocation")" > "$configFileLocation"
-          echo "$(pcregrep -Mv "\[${matchingExpired[i]}\]\n((aws_access_key_id|aws_secret_access_key).*\n)*aws_session_token.*" "$credentialFileLocation")" > "$credentialFileLocation"
-        done
-        echo "ALL profiles are deleted."
       else
-        echo "No profile deleted."
+        echo "Profile MFA session expirated no found."
       fi
     else
-      echo "Profile MFA session expirated no found."
+      echo "You have no profile MFA session."
     fi
-  else
-    echo "You have no profile MFA session."
   fi
 fi
 
-#Get some info by the user
-read -p "Name of profile to CREATE (enter to generate automatically): " pname
+if [ $interactiveMode == true ]; then
+  #Get some info by the user
+  read -p "Name of profile to CREATE (enter to generate automatically): " pname
+fi
 
 #Check if AWS_PROFILE is configured
 if [ -z $AWS_PROFILE ]; then 
-  echo "List of profiles configurated : "
-  while read line; do
-        echo "$line"
-  done <<< "$(grep "\[.*\]" "$credentialFileLocation" | sed "s/[]]//g" | sed "s/[[]//g")";
+  if [ $interactiveMode == true ]; then
+    echo "List of profiles configurated : "
+    while read line; do
+      echo "$line"
+    done <<< "$(grep "\[.*\]" "$credentialFileLocation" | sed "s/[]]//g" | sed "s/[[]//g")";
 
-  read -p "Name of profile to fetch credentials (enter to use default profile): " profile
+    read -p "Name of profile to fetch credentials (enter to use default profile): " profile
+  fi
 else
   echo "DETECTED : Profile with AWS_PROFILE and it will be use : $AWS_PROFILE"
   profile="$AWS_PROFILE"
@@ -106,10 +133,17 @@ fi
 arnmfa=$(aws iam list-virtual-mfa-devices --profile $profile | jq ".VirtualMFADevices[].SerialNumber" | grep mfa/$username | sed 's/"//g')
 if [ -z $arnmfa ]; then echo "No found MFA. You need to create one." && exit 1; fi
 
-read -p "Enter your MFA token : " tokenMFA
+if [ $interactiveMode == "true" ]; then
+  read -p "Enter your MFA token : " tokenMFA
+fi
 
 #Get Token
 credentials=$(aws sts get-session-token --serial-number $arnmfa --token-code $tokenMFA --profile $profile)
+if [ $? -ne 0 ]; then
+  echo "Invalid Token."
+  exit 1
+fi
+
 sak=$(echo $credentials | jq '.Credentials.SecretAccessKey' | sed 's/"//g')
 ak=$(echo $credentials | jq '.Credentials.AccessKeyId' | sed 's/"//g')
 sessionToken=$(echo $credentials | jq '.Credentials.SessionToken' | sed 's/"//g')
--- readme.md
+++ readme.md
@@ -1,12 +1,14 @@
 # Tools for aws utility
 
 ## aws-getTokenMFA.sh
-Retreive a token MFA and put it on your credentials file 
+Retreive a token MFA and put it on your credentials file
+./aws-getTokenMFA.sh MFAtoken [profileName] [ProfileFetchCredentials] 
+- [X] CAN : Be use with arg (no interactive mode)
 - [X] CAN : Check if a session profile is expired
 - [X] CAN : If the you want, you can delete 1 or multiple session profiles if expired
 - [X] List profiles if AWS_PROFILE is not specify
 - [X] Select AWS_DEFAULT_PROFILE if the user want to use default profile
-- [X] Retreive the token session with the profile selected
+- [X] Retreive the token session with the profile selected if the MFA token is valid
 - [X] Update your credentials and config with a new profile and the token session
 
 ## aws-listProfiles.sh
